b10c8e766777bf6737e5c7d8c2a68a0de189f5b9
Sehrish Javed <sehrish@example.com>
2025-08-01 17:09:13 +0500

    Practice set added

--- 01_Basics_of_OOP/01_Introduction/DataMemeber_DataFunction/Practice_01.cpp
+++ 01_Basics_of_OOP/01_Introduction/DataMemeber_DataFunction/Practice_01.cpp
@@ -1,1 +1,21 @@
-
+// Write a program that declarea a class with one integer data member and two member functions in() and out() to input data in data member.
+#include <iostream>
+using namespace std;
+class Test { // Define a class named Test
+private:
+    int n;// Data member: holds an integer value
+public:
+    void in() {
+        cout << "Enter an integer: ";
+        cin >> n;// Store input in data member 'n'
+    }
+    void out() { //Member function to display the stored integer
+        cout << "The integer is: " << n << endl;
+    }
+};
+int main() {
+    Test obj;// Create an object of class Test
+    obj.in();// Call the in() function to take input
+    obj.out();// Call the out() function to display the value
+    return 0;// Indicate successful program termination
+}
