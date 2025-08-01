ab9efb715543d8fb563549e6eda0c38a98ebd1fa
Sehrish Javed <sehrish@example.com>
2025-08-01 19:03:47 +0500

    Practice set added

--- 01_Basics_of_OOP/01_Introduction/DataMemeber_DataFunction/Practice_02.cpp
+++ 01_Basics_of_OOP/01_Introduction/DataMemeber_DataFunction/Practice_02.cpp
@@ -1,1 +1,38 @@
+// Write a class Marks with three data members to store three marks.Write three member functions in() to input marks, sum() to calculate and return the sum and avg() to calculate and return the average of the marks. 
+#include <iostream>
+using namespace std;
+class Marks {
+    private:
+    int a, b, c;
+    public:
+    void in(){
+        cout<<"Enter Your Marks of sub1: ";
+        cin>>a;
+        cout<<"Enter Your Marks of sub2: ";
+        cin>>b;
+        cout<<"Enter Your Marks of sub3: ";
+        cin>>c;
+    
+    }
+    int sum(){
+        return a+b+c;
+    }
+    float avg(){
+        return (a+b+c)/3.0;
+    }
 
+};
+int main()
+{
+    Marks m;
+    int s;
+    float a;
+    m.in();
+    s = m.sum();
+    cout<<"Sum = "<<s<<endl;
+    a = m.avg();
+
+    cout<<"Average = "<<a<<endl;
+    return 0;
+
+}
